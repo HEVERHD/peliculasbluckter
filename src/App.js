5a964361ab6aefaefd12ce6d490f29667ee5a6f1
unknown <hever11.hdgd@gmail.com>
2022-08-11 16:44:41 -0500

    x

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import Pelicula from "./components/peliculas";
 import Wrapper from "./components/Wrapper";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import Paginacion from "./components/Paginacion";
 
 function App() {
   const [peliculas, setPeliculas] = useState([]);
+  const [paginaActual, setPaginaActual] = useState(1);
+
+  let peliculasAll = setPeliculas;
+
+  peliculasAll = peliculas.slice((paginaActual - 1) * 5, paginaActual * 10);
+  console.log(peliculasAll);
 
   useEffect(() => {
     const enPoint =
@@ -40,6 +47,14 @@ function App() {
             ></Pelicula>
           </>
         ))}
+
+        <Paginacion
+          pagina={paginaActual}
+          total={4}
+          cambiarPagina={(pagina) => {
+            setPaginaActual(pagina);
+          }}
+        />
       </Wrapper>
     </>
   );
